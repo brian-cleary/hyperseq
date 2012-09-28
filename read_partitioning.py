3e0703e273a18a6c25df403fff3de3faa7059968
brian-cleary <cleary.brian@gmail.com>
2012-09-28 10:20:50 -0400

    test for read memberhsip in arbitrary hash (not efficient)

--- read_partitioning.py
+++ read_partitioning.py
@@ -1,7 +1,9 @@
-from vdb_reader import *
+#from vdb_reader import *
+from fastq_reader import *
 from hyper_sequences import affine_hull,letters_to_coords,generator_to_bins,get_wheels
 import cPickle
 from bitarray import bitarray
+import math
 
 def set_wheels(f,num_dimensions,spokes=41,wheels=5,out_path='/mnt/'):
 	Wheels = []
@@ -34,15 +36,43 @@ def create_kmer_hash(f,s,w=1,wheel_path='/mnt/Wheels.txt',block_size=10000,out_p
 	k = len(W[0]['p'])
 	f.seek(0)
 	H = bitarray(2**s)
+	# APPARENTLY BITARRAY IS NOT GUARANTEED TO INITIALIZE EMPTY
+	H.setall(False)
 	last = None
 	while last != f.tell():
 		last = f.tell()
-		A,B = generator_to_bins(read_generator(f,max_reads=block_size,kmer_size=k),W)
-		for b in range(len(B)):
-			for a in range(len(A)):
-				H[B[b][a]] = True
+		try:
+			A,B = generator_to_bins(read_generator(f,max_reads=block_size,kmer_size=k),W)
+			for b in range(len(B)):
+				for a in range(len(A)):
+					H[B[b][a]] = True
+		except:
+			pass
 		print f.tell()
 	fo = open(out_path,'wb')
 	H.tofile(fo)
 	fo.close()
 	return H
+
+# NOT THE MOST EFFICIENT THING EVER...PASSING OVER AND HASHING ALL DATA AGAIN
+def membership_generator(f,H,wheel_path='/mnt/Wheels.txt',block_size=10000):
+	s = math.log(len(H),2)
+	W = get_wheels(wheel_path,spoke_limit=s,wheel_limit=1)
+	k = len(W[0]['p'])
+	f.seek(0)
+	last = None
+	while last != f.tell():
+		last = f.tell()
+		try:
+			A,B = generator_to_bins(read_generator(f,max_reads=block_size,verbose_ids=True,kmer_size=k),W)
+			reads_found = {}
+			for b in range(len(B)):
+				for a in range(len(A)):
+					if H[B[b][a]]:
+						# A SIMPLETONS METHOD FOR ONLY WRITING EACH READ ONCE
+						if A[a] not in reads_found:
+							reads_found[A[a]] = True
+							yield A[a]
+		except Exception, err:
+			print str(err)
+		print f.tell()
