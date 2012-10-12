2aa600f07bb297e2c4a132f44c3d574c53e3e161
brian-cleary <cleary.brian@gmail.com>
2012-10-12 09:32:06 -0400

    create a counting hash

--- read_partitioning.py
+++ read_partitioning.py
@@ -4,6 +4,7 @@ from hyper_sequences import affine_hull,letters_to_coords,generator_to_bins,get_
 import cPickle
 from bitarray import bitarray
 import math
+from ctypes import c_uint8
 
 def set_wheels(f,num_dimensions,spokes=41,wheels=5,out_path='/mnt/'):
 	Wheels = []
@@ -54,9 +55,38 @@ def create_kmer_hash(f,s,w=1,wheel_path='/mnt/Wheels.txt',block_size=10000,out_p
 	fo.close()
 	return H
 
+def create_kmer_hash_counts(f,s,w=1,wheel_path='/mnt/Wheels.txt',block_size=10000,out_path='/mnt/Kmer_Hash_Counts.txt'):
+	W = get_wheels(wheel_path,spoke_limit=s,wheel_limit=w)
+	k = len(W[0]['p'])
+	f.seek(0)
+	H = (c_uint8*2**s)()
+	last = None
+	while last != f.tell():
+		last = f.tell()
+		try:
+			A,B = generator_to_bins(read_generator(f,max_reads=block_size,kmer_size=k),W)
+			for b in range(len(B)):
+				for a in range(len(A)):
+					# would be great to have overflow checking
+					H[B[b][a]] = min(255,H[B[b][a]]+1)
+		except Exception,err:
+			print str(err)
+		print f.tell()
+	fo = open(out_path,'wb')
+	fo.write(H)
+	fo.close()
+	return H
+
+def open_count_hash(file_path,s):
+	f = open(file_path,'rb')
+	H = (c_uint8*2**s)()
+	f.readinto(H)
+	f.close()
+	return H
+
 # NOT THE MOST EFFICIENT THING EVER...PASSING OVER AND HASHING ALL DATA AGAIN
 def membership_generator(f,H,wheel_path='/mnt/Wheels.txt',block_size=10000):
-	s = math.log(len(H),2)
+	s = int(math.log(len(H),2))
 	W = get_wheels(wheel_path,spoke_limit=s,wheel_limit=1)
 	k = len(W[0]['p'])
 	f.seek(0)
