d4a7287f723655574a29b4976f8268cf7e28954c
brian-cleary <cleary.brian@gmail.com>
2012-11-05 14:18:33 -0500

    use sets in membership generator

--- read_partitioning.py
+++ read_partitioning.py
@@ -5,6 +5,7 @@ import cPickle
 from bitarray import bitarray
 import math
 from ctypes import c_uint8
+from collections import defaultdict
 
 def set_wheels(f,num_dimensions,spokes=41,wheels=5,out_path='/mnt/'):
 	Wheels = []
@@ -31,6 +32,30 @@ def rand_kmer_coords(f,nodes):
 		if rk['q'].count(2) < 3:
 			N[len(N)] = list(letters_to_coords(rk))
 	return N
+
+def write_hashed_reads(read_file,out_file,s,w=1,wheel_path='/mnt/Wheels.txt',block_size=10000):
+	W = get_wheels(wheel_path,spoke_limit=s,wheel_limit=w)
+	k = len(W[0]['p'])
+	hash_prefix = 'k, bins: '
+	read_file.seek(0)
+	last = None
+	while last != read_file.tell():
+		last = read_file.tell()
+		try:
+			A,B = generator_to_bins(read_generator(read_file,max_reads=block_size,verbose_ids=True,kmer_size=k),W)
+			# WRITING JUST ONE WHEEL HERE, ASSUMING SORTED BY READ
+			B0 = []
+			last_a = None
+			for a in range(len(A)):
+				if A[a] != last_a:
+					if B0:
+						out_file.write(last_a+hash_prefix+str([k] + B0)+'\n')
+						B0 = []
+					last_a = A[a]
+				B0.append(B[0][a])
+		except Exception, err:
+			print Exception,str(err)
+		print read_file.tell()
 
 def create_kmer_hash(f,s,w=1,wheel_path='/mnt/Wheels.txt',block_size=10000,out_path='/mnt/Kmer_Hash.txt'):
 	W = get_wheels(wheel_path,spoke_limit=s,wheel_limit=w)
@@ -84,25 +109,27 @@ def open_count_hash(file_path,s):
 	f.close()
 	return H
 
-# NOT THE MOST EFFICIENT THING EVER...PASSING OVER AND HASHING ALL DATA AGAIN
-def membership_generator(f,H,wheel_path='/mnt/Wheels.txt',block_size=10000):
-	s = int(math.log(len(H),2))
+# DUMB: match_thresh SHOULD PROBABLY BE DETERMINED FROM READS
+def membership_generator(f,H,wheel_path='/mnt/Wheels.txt',block_size=10000,match_thresh=35):
+	s = int(math.log(len(H[0]),2))
 	W = get_wheels(wheel_path,spoke_limit=s,wheel_limit=1)
-	k = len(W[0]['p'])
 	f.seek(0)
 	last = None
 	while last != f.tell():
 		last = f.tell()
 		try:
-			A,B = generator_to_bins(read_generator(f,max_reads=block_size,verbose_ids=True,kmer_size=k),W)
-			reads_found = {}
-			for b in range(len(B)):
-				for a in range(len(A)):
-					if H[B[b][a]]:
-						# A SIMPLETONS METHOD FOR ONLY WRITING EACH READ ONCE
-						if A[a] not in reads_found:
-							reads_found[A[a]] = True
-							yield A[a]
+			for a in read_generator(f,max_reads=block_size,verbose_ids=True,from_hashq=True):
+				read_set = set(a[2])
+				for h in range(len(H)):
+					if len(read_set & H[h]) >= match_thresh:
+						yield h,a[0]
 		except Exception, err:
 			print str(err)
 		print f.tell()
+
+def bitarray_from_array(A,s):
+	H = bitarray(2**s)
+	H.setall(False)
+	for a in A:
+		H[a] = True
+	return H
